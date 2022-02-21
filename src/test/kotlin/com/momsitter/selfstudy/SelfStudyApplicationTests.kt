c7dede766141659b2c72620d077e2f25d5bac755
Rok93 <kr.kim@mfort.co.kr>
2022-02-22 01:26:28 +0900

    Initial commit

---
+++ src/test/kotlin/com/momsitter/selfstudy/SelfStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.momsitter.selfstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SelfStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
